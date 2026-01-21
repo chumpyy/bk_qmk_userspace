cac1dab42a05bb2d7ea9504477b8133382f7164e
cabral <paulo.cabral@itu.int>
2026-01-21 22:20:58 +0100

    boot on double tap

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/personal/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/personal/keymap.c
@@ -67,7 +67,8 @@ enum dilemma_keymap_layers {
 enum {
     TD_0,
     TD_1,
-    TD_2
+    TD_2,
+    TD_3_BOOT,
 };
 
 void td_0_finished(tap_dance_state_t *state, void *user_data) {
@@ -121,7 +122,8 @@ void td_3_reset(tap_dance_state_t *state, void *user_data) {
 tap_dance_action_t tap_dance_actions[] = {
     [TD_0] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_0_finished, td_0_reset),
     [TD_1] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_1_finished, td_1_reset),
-    [TD_2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_2_finished, td_2_reset)
+    [TD_2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_2_finished, td_2_reset),
+    [TD_3_BOOT] = ACTION_TAP_DANCE_DOUBLE(QK_BOOT, KC_LSFT),         
 };
 
 /*tap_dance_action_t tap_dance_actions[] = {
@@ -154,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        _______, TD(TD_0), TD(TD_1), TD(TD_2), C(KC_F), KC_F3,    KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_DEL, KC_BSLS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       QK_BOOT, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_F12,    KC_PGUP,LCS(KC_LEFT),_______,LCS(KC_RIGHT),KC_PGDN, QK_BOOT,
+ TD(TD_3_BOOT), C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_F12,    KC_PGUP,LCS(KC_LEFT),_______,LCS(KC_RIGHT),KC_PGDN, TD(TD_3_BOOT),
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                          _______, XXXXXXX, KC_NO,   _______,    KC_LSFT,   KC_BSPC, XXXXXXX, KC_LGUI
   //                    ╰───────────────────────────────────╯ ╰───────────────────────────────────╯

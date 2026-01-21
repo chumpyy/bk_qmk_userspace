81a302c1862ab4a2f3f15b454c843acd3b8aa717
cabral <paulo.cabral@itu.int>
2026-01-21 23:10:47 +0100

    Fix tap dance with qmk documented general tap-hold

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/personal/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/personal/keymap.c
@@ -65,72 +65,73 @@ enum dilemma_keymap_layers {
 
 // Tap Dance
 enum {
-    TD_0,
+    TD_0 = 0,
     TD_1,
     TD_2,
     TD_3_BOOT,
 };
 
-void td_0_finished(tap_dance_state_t *state, void *user_data) {
-    if (state->pressed && !state->interrupted) {
-        register_code(KC_LSFT);
-    } else {
-        tap_code16(LCTL(KC_A));
+typedef struct {
+    uint16_t tap;
+    uint16_t hold;
+    uint16_t held;
+} tap_dance_tap_hold_t;
+
+void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (state->pressed) {
+        if (state->count == 1
+#ifndef PERMISSIVE_HOLD
+            && !state->interrupted
+#endif
+        ) {
+            register_code16(tap_hold->hold);
+            tap_hold->held = tap_hold->hold;
+        } else {
+            register_code16(tap_hold->tap);
+            tap_hold->held = tap_hold->tap;
+        }
     }
 }
 
-void td_0_reset(tap_dance_state_t *state, void *user_data) {
-    unregister_code(KC_LSFT);
-}
+void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
 
-void td_1_finished(tap_dance_state_t *state, void *user_data) {
-    if (state->pressed && !state->interrupted) {
-        register_code(KC_LSFT);
-    } else {
-        tap_code16(LCTL(KC_A));
+    if (tap_hold->held) {
+        unregister_code16(tap_hold->held);
+        tap_hold->held = 0;
     }
 }
 
-void td_1_reset(tap_dance_state_t *state, void *user_data) {
-    unregister_code(KC_LSFT);
-}
-
-void td_2_finished(tap_dance_state_t *state, void *user_data) {
-    if (state->pressed && !state->interrupted) {
-        register_code(KC_LCTL);
-    } else {
-        tap_code16(LCTL(KC_S));
+#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold)                                        \
+    {                                                                               \
+        .fn        = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, \
+        .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}),               \
     }
-}
-
-void td_2_reset(tap_dance_state_t *state, void *user_data) {
-    unregister_code(KC_LCTL);
-}
-
-void td_3_finished(tap_dance_state_t *state, void *user_data) {
-    if (state->pressed && !state->interrupted) {
-        register_code(KC_LGUI);
-    } else {
-        tap_code16(LCTL(KC_F3));
-    }
-}
-
-void td_3_reset(tap_dance_state_t *state, void *user_data) {
-    unregister_code(KC_LGUI);
-}
 
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_0] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_0_finished, td_0_reset),
-    [TD_1] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_1_finished, td_1_reset),
-    [TD_2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_2_finished, td_2_reset),
-    [TD_3_BOOT] = ACTION_TAP_DANCE_DOUBLE(QK_BOOT, KC_LSFT),         
+     [TD_0] = ACTION_TAP_DANCE_TAP_HOLD(LCTL(KC_A), KC_LSFT),
+     [TD_1] = ACTION_TAP_DANCE_TAP_HOLD(LCTL(KC_S), KC_LCTRL),
+     [TD_2] = ACTION_TAP_DANCE_TAP_HOLD(LCTL(KC_F3), KC_LGUI),
+     [TD_3_BOOT] = ACTION_TAP_DANCE_DOUBLE(QK_BOOT, KC_LSFT)
 };
 
-/*tap_dance_action_t tap_dance_actions[] = {
-     [TD_0] = ACTION_TAP_DANCE_DOUBLE(C(KC_A), KC_LSFT),         
-     [TD_1] = ACTION_TAP_DANCE_DOUBLE(C(KC_S), KC_LCTL),
-     [TD_2] = ACTION_TAP_DANCE_DOUBLE(S(KC_F3), KC_LGUI) // (x, y) tap once for x, tap twice for y
-};*/
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    tap_dance_action_t *action;
+    tap_dance_state_t* state;
+
+    switch (keycode) {
+        case TD(CT_CLN):
+            action = tap_dance_get(QK_TAP_DANCE_GET_INDEX(keycode));
+            state = tap_dance_get_state(QK_TAP_DANCE_GET_INDEX(keycode));
+            if (!record->event.pressed && state != NULL && state->count && !state->finished) {
+                tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+                tap_code16(tap_hold->tap);
+            }
+    }
+    return true;
+}
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {

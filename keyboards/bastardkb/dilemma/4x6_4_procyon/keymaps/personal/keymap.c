473ab891e09ad9dadec77e2b23214920384114ae
cabral <paulo.cabral@itu.int>
2026-01-21 22:14:22 +0100

    Fix tap dance

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/personal/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/personal/keymap.c
@@ -49,9 +49,9 @@ enum dilemma_keymap_layers {
 #define T_CS_CAPS LCS_T(KC_CAPS)
 // #define T_CS_CAPS LCTL_T(CW_TOGG) //caps word
 
-#define T_G_S_F3 MT(MOD_LGUI, S(KC_F3))
-#define T_S_C_A MT(MOD_LSFT, LCTL(KC_A))
-#define T_C_C_S MT(MOD_LCTL, LCTL(KC_S))
+// #define T_S_C_A MT(MOD_LSFT, LCTL(KC_A))
+// #define T_C_C_S MT(MOD_LCTL, LCTL(KC_S))
+// #define T_G_S_F3 MT(MOD_LGUI, S(KC_F3))
 
 #define PT_Z LT(LAYER_POINTER, KC_Z)
 #define PT_SLSH LT(LAYER_POINTER, KC_SLSH)
@@ -64,16 +64,70 @@ enum dilemma_keymap_layers {
 #endif // !POINTING_DEVICE_ENABLE
 
 // Tap Dance
-/*enum {
+enum {
     TD_0,
     TD_1,
     TD_2
 };
 
+void td_0_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->pressed && !state->interrupted) {
+        register_code(KC_LSFT);
+    } else {
+        tap_code16(LCTL(KC_A));
+    }
+}
+
+void td_0_reset(tap_dance_state_t *state, void *user_data) {
+    unregister_code(KC_LSFT);
+}
+
+void td_1_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->pressed && !state->interrupted) {
+        register_code(KC_LSFT);
+    } else {
+        tap_code16(LCTL(KC_A));
+    }
+}
+
+void td_1_reset(tap_dance_state_t *state, void *user_data) {
+    unregister_code(KC_LSFT);
+}
+
+void td_2_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->pressed && !state->interrupted) {
+        register_code(KC_LCTL);
+    } else {
+        tap_code16(LCTL(KC_S));
+    }
+}
+
+void td_2_reset(tap_dance_state_t *state, void *user_data) {
+    unregister_code(KC_LCTL);
+}
+
+void td_3_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->pressed && !state->interrupted) {
+        register_code(KC_LGUI);
+    } else {
+        tap_code16(LCTL(KC_F3));
+    }
+}
+
+void td_3_reset(tap_dance_state_t *state, void *user_data) {
+    unregister_code(KC_LGUI);
+}
+
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_0] = ACTION_TAP_DANCE_DOUBLE(S(KC_F3), KC_LGUI), // (x, y) tap once for x, tap twice for y
-    [TD_1] = ACTION_TAP_DANCE_DOUBLE(C(KC_A), KC_LSFT),
-    [TD_2] = ACTION_TAP_DANCE_DOUBLE(C(KC_S), KC_LCTL)
+    [TD_0] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_0_finished, td_0_reset),
+    [TD_1] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_1_finished, td_1_reset),
+    [TD_2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_2_finished, td_2_reset)
+};
+
+/*tap_dance_action_t tap_dance_actions[] = {
+     [TD_0] = ACTION_TAP_DANCE_DOUBLE(C(KC_A), KC_LSFT),         
+     [TD_1] = ACTION_TAP_DANCE_DOUBLE(C(KC_S), KC_LCTL),
+     [TD_2] = ACTION_TAP_DANCE_DOUBLE(S(KC_F3), KC_LGUI) // (x, y) tap once for x, tap twice for y
 };*/
 
 // clang-format off
@@ -98,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        _______, KC_F2,   C(KC_F5),G(KC_E), LCA(KC_R),C(KC_T),  C(KC_Y), KC_HOME, KC_UP,   KC_END, KC_NO, S(KC_BSLS),
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       _______, T_S_C_A, T_C_C_S, T_G_S_F3, C(KC_F), KC_F3,    KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_DEL, KC_BSLS,
+       _______, TD(TD_0), TD(TD_1), TD(TD_2), C(KC_F), KC_F3,    KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_DEL, KC_BSLS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        QK_BOOT, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_F12,    KC_PGUP,LCS(KC_LEFT),_______,LCS(KC_RIGHT),KC_PGDN, QK_BOOT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯

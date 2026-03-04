1d8cb1d3030f4bb3c6cd683f9ce0380e48c0f07f
cabral <paulo.cabral@itu.int>
2026-03-04 12:02:59 +0100

    Trying one shot

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/personal/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/personal/keymap.c
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
   KC_KB_VOLUME_DOWN, KC_F1, KC_F2, KC_F3,   KC_F4,   KC_F5,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                         KC_NO,    XXXXXXX,   KC_LGUI,KC_KB_MUTE,  T_G_ENT, C(KC_LALT), XXXXXXX,   KC_NO
+                         KC_NO,    XXXXXXX,   KC_LGUI,KC_KB_MUTE,  T_G_ENT, OS_LCAG, XXXXXXX,   KC_NO
   //                    ╰───────────────────────────────────╯ ╰───────────────────────────────────╯
   ),
 
